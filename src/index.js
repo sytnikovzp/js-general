35ef89428c47eead816157cf80249ef2f6420d6a
alex <sytnikov.zp@gmail.com>
2024-01-27 12:50:27 +0200

    Add types

---
+++ src/index.js
@@ -0,0 +1,42 @@
+"use strict";
+
+// let abc = "str";
+
+// console.log(abc);
+
+// let a = 30,
+//   d = 40,
+//   e = 50;
+// const b = 20;
+
+// console.log(a + b);
+// console.log(a);
+
+// let getFirstName = (null){};
+
+// const MATH_PI = Math.PI;
+// const COLOR = "#0f0a00";
+
+
+// console.log(2 ** 53 -1);
+
+let num = 0;
+let bigNum = 20n;
+let str = '';
+
+// const user = {
+//   name: 'John',
+//   age: 20, 
+//   isMarried: true,
+//   address: {
+//     street: 'ddg',
+//     city: 'fogdfg',
+//   },
+// };
+
+console.log(typeof bigNum);
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof null);
+console.log(typeof NaN);
+console.log(2 / 0);
