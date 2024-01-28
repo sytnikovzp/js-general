1bdf697e32c79a70370667dce42674c3fd5350d5
alex <sytnikov.zp@gmail.com>
2024-01-28 12:14:21 +0200

    With && and ||

--- src/op-loops.js
+++ src/op-loops.js
@@ -9,16 +9,32 @@ console.log(year);
 // Strangeness of isNaN()
 // console.log(Number.isNaN(NaN));
 
+// if (Number.isNaN(year)){
+//   console.log('Entered wrong data');
+// } else if (year % 100 === 0){
+//     if(year % 400 === 0){
+//       console.log('This is a leap year');
+//     } else {
+//       console.log('This is not a leap year');
+//     }
+// } else if (year % 4 === 0 && year > 4){
+//       console.log('This is a leap year')
+// } else {
+//   console.log('This is not a leap year');
+// }
+
+// With && and ||
+
 if (Number.isNaN(year)){
-  console.log('Entered wrong data');
-} else if (year % 100 === 0){
-    if(year % 400 === 0){
-      console.log('This is a leap year');
-    } else {
-      console.log('This is not a leap year');
-    }
-} else if (year % 4 === 0 && year > 4){
-      console.log('This is a leap year')
-} else {
-  console.log('This is not a leap year');
-}
+    console.log('Entered wrong data');
+  } else {
+    year % 4 === 0 && 
+    (year % 100 !== 0 || year % 400 === 0) &&
+      year > 4 &&
+      console.log('It is a leap year');
+
+      (year % 4 === 0 && 
+        (year % 100 !== 0 || year % 400 === 0) &&
+        year > 4) || 
+        console.log('This is not a leap year');
+  }
