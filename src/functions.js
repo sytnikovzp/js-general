2f9efd80f2279c0982f0d57e148c909a49956f8b
alex <sytnikov.zp@gmail.com>
2024-02-03 11:10:01 +0200

    Add arrow functions

--- src/functions.js
+++ src/functions.js
@@ -1,19 +1,45 @@
 'use strict';
 
-//function declaration
-function summ(a, b) {
-  // parameters
-  // console.log(a + b);
-  return a + b;
+// //function declaration
+// function summ(a, b) {
+//   // parameters
+//   // console.log(a + b);
+//   return a + b;
+// }
+
+// console.log(summ(10, 20)); // argumets
+
+// // function expression
+// const showDivide = function (c, d) {
+//   return c / d;
+// };
+// console.log(showDivide(20, 10)); // without "" is body func
+
+// // const newFunc = summ;
+// // console.log(newFunc(2, 3));
+
+// Check Age
+
+let age = 20;
+let welcome = function () {};
+
+if (age < 18) {
+  function welcome() {
+    console.log('you are too much young');
+  }
+  welcome();
+} else {
+  function welcome() {
+    console.log('you are enough adult');
+  }
+  welcome();
 }
 
-console.log(summ(10, 20)); // argumets
+// welcome();
 
-// function expression
-const showDivide = function (c, d) {
-  return c / d;
+// Arrow function
+
+const function3 = (par) => {
+  console.log(par);
+  return par * 2;
 };
-console.log(showDivide(20, 10)); // without "" is body func
-
-// const newFunc = summ;
-// console.log(newFunc(2, 3));
