8cefb7aee0b831e7376dc28cf465c0c3774e181d
alex <sytnikov.zp@gmail.com>
2024-02-04 13:37:44 +0200

    Add constructor

--- src/objects.js
+++ src/objects.js
@@ -46,22 +46,22 @@
 
 // Car
 
-let color = 'green';
+// let color = 'green';
 
-const car = {
-  brand: 'Toyota',
-  model: 'avalon',
-  'year of relise': 1995,
-  transmission: 'mechanics',
-  isWell: true,
-  color,
-  run(driver) {
-    return `Run and drive ${driver} on ${this.brand} ${this.model}`;
-  },
-  go: (driver) => {
-    return `Run and drive ${driver} on ${this.brand} ${this.model}`;
-  },
-};
+// const car = {
+//   brand: 'Toyota',
+//   model: 'avalon',
+//   'year of relise': 1995,
+//   transmission: 'mechanics',
+//   isWell: true,
+//   color,
+//   run(driver) {
+//     return `Run and drive ${driver} on ${this.brand} ${this.model}`;
+//   },
+//   go: (driver) => {
+//     return `Run and drive ${driver} on ${this.brand} ${this.model}`;
+//   },
+// };
 
 // console.log(car);
 
@@ -86,22 +86,22 @@ const car = {
 
 // Copy of object
 
-const car2 = {
-  brand: 'Toyota',
-  model: 'avalon',
-  'year of relise': 1995,
-  transmission: 'mechanics',
-  isWell: true,
-  color,
-  run(driver) {
-    return `Run and drive ${driver}`;
-  },
-  1: 1,
-};
+// const car2 = {
+//   brand: 'Toyota',
+//   model: 'avalon',
+//   'year of relise': 1995,
+//   transmission: 'mechanics',
+//   isWell: true,
+//   color,
+//   run(driver) {
+//     return `Run and drive ${driver}`;
+//   },
+//   1: 1,
+// };
 
 // console.log(car2);
 
-const car3 = car;
+// const car3 = car;
 // console.log(car3);
 
 // console.log(car === car2); // compare address, copy by address
@@ -131,39 +131,58 @@ const car3 = car;
 
 // THIS
 
-const desktop = {
-  brand: 'Intel',
-};
+// const desktop = {
+//   brand: 'Intel',
+// };
 
-const laptop = {
-  brand: 'Dell',
-};
+// const laptop = {
+//   brand: 'Dell',
+// };
 
-function showBrand() {
-  return this.brand;
+// function showBrand() {
+//   return this.brand;
+// }
+
+// function func() {
+//   return this;
+// }
+
+// const arrowFunc = () => this;
+
+// const bindArrow = func.bind(laptop);
+
+// // laptop.show = arrowFunc;
+// // desktop.show = func;
+
+// laptop.show = showBrand;
+// desktop.show = showBrand;
+
+// // console.log(func());
+// // console.log(arrowFunc());
+
+// console.log(laptop.show());
+// console.log(desktop.show.call(car));
+
+// const bindShow = laptop.show.bind(car);
+
+// // console.log(typeof bindShow)
+// console.log(bindShow());
+
+// CONSTRUCTOR
+
+function Car(brand, model, transmission = 'auto', color = 'white') {
+  this.brand = brand;
+  this.model = model;
+  this.transmission = transmission;
+  this.color = color;
+  this.run = function () {
+    return `Run and drive on ${this.brand} ${this.model}`;
+  };
 }
 
-function func() {
-  return this;
-}
+const bmw = new Car('BMW', 'X5', 'auto', 'black');
+const toyota = new Car('Toyota', 'Camry');
 
-const arrowFunc = () => this;
 
-const bindArrow = func.bind(laptop);
-
-// laptop.show = arrowFunc;
-// desktop.show = func;
-
-laptop.show = showBrand;
-desktop.show = showBrand;
-
-// console.log(func());
-// console.log(arrowFunc());
-
-console.log(laptop.show());
-console.log(desktop.show.call(car));
-
-const bindShow = laptop.show.bind(car);
-
-// console.log(typeof bindShow)
-console.log(bindShow());
+console.log(bmw);
+console.log(toyota)
