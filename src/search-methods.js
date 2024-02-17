f667469d5595b1c41436047370f54b53016a6b1a
alex <sytnikov.zp@gmail.com>
2024-02-17 11:38:09 +0200

    Add method MAP

--- src/search-methods.js
+++ src/search-methods.js
@@ -1,62 +1,60 @@
 'use strict';
 
-function Player(fistName, lastName, foreHand, backHand, age, rate, city) {
-  this.fistName = fistName;
-  this.lastName = lastName;
-  this.foreHand = foreHand;
-  this.backHand = backHand;
-  this.age = age;
-  this.rate = rate;
-  this.city = city;
-}
-const maLong = new Player(
-  'Ma',
-  'Long',
-  'Butterfly Tenergy 19',
-  'Donic Acuda S1',
-  45,
-  40,
-  'Dnipro'
-);
-const xuXin = new Player(
-  'Xu',
-  'Xin',
-  'Butterfly Tenergy 05',
-  'Stiga Calibra',
-  30,
-  38,
-  'Kiev'
-);
-const kokiNiva = new Player(
-  'Koki',
-  'Niva',
-  'DHS Huricane',
-  'TSP',
-  25,
-  35,
-  'Odessa'
-);
-const players = [maLong, xuXin, kokiNiva];
+// methods array (perebor)
 
-function getProps(limAge) {
-  if (this.age > limAge) {
-    console.log('This player is too old');
-  } else {
-    for (const key in this) {
-      console.log(`${key} = ${this[key]}`);
-    }
+// --- Method forEach ---
+
+// let a;
+const arr1 = [1, 2, 3];
+
+arr1.forEach((el, i, arr1) => {
+  // console.log(el * 5);
+  // arr1[i] = el * 5;
+});
+
+// console.log(arr1);
+
+// Own implementation of forEach
+
+function myForEach(arr, callback, thisArg) {
+  for (let i = 0; i < arr.length; i++) {
+    callback.call(thisArg, arr[i], i, arr);
   }
 }
 
-const getPropsWithThis = getProps.bind(maLong, 35);
+myForEach(arr1, (el) => console.log(el));
 
-const limAge = 40;
+// --- Method find ---
 
-for (const player of players) {
-  getProps.call(player, limAge);
-  console.log('===================');
+const arr2 = ['Donic', 'Stiga', 'DHS', 'Xiom', 'TSP'];
+
+// console.log(arr2.find((el) => el === 'DHS'));
+// console.log(arr2.findIndex((el) => el === 'DHS'));
+// console.log(arr2.findIndex((el) => el === 'DHSs'));
+
+const arr3 = [1, 2, 3, 4, 5];
+
+// console.log(arr3.find((el) => el % 2 === 0));
+// console.log(arr3.findLastIndex((el) => el % 2 === 0));
+
+// --- Method map ---
+
+const strIntArr = ['1', '5', '10'];
+
+const parseToInt = (Item) => parseInt(Item) * 10;
+
+const intArr = strIntArr.map(parseToInt);
+
+// console.log(intArr);
+
+// Own implementation of map
+
+function myMap(arr, callback, thisArg) {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    result.push(callback.apply(thisArg, [arr[i], i, arr]));
+  }
+  return result;
 }
 
-
-getPropsWithThis();
-
+console.log(myMap(strIntArr, parseToInt));
