f5f14a12d09af1dd7a0228f63eaf0ab4e7f6301c
alex <sytnikov.zp@gmail.com>
2024-02-18 11:35:34 +0200

    Add rest and spread

--- src/prototype.js
+++ src/prototype.js
@@ -1,28 +1,54 @@
 'use strict';
 
-function Country(title = 'Ukraine', capital = 'Kiev') {
-  this.title = title;
-  this.capital = capital;
-  // this.getInfo = function () {
-  //   return `Capital is ${this.capital}`;
-  // };
+// function Country(title = 'Ukraine', capital = 'Kiev') {
+//   this.title = title;
+//   this.capital = capital;
+//   // this.getInfo = function () {
+//   //   return `Capital is ${this.capital}`;
+//   // };
+// }
+
+// Country.prototype.getInfo = function () {
+//   return `Capital is ${this.capital}`;
+// };
+
+// const ukraine = new Country();
+// const usa = new Country('USA', 'Washington');
+
+// console.log(ukraine.getInfo());
+// console.log(usa.getInfo());
+
+// console.log(ukraine.getInfo === usa.getInfo);
+
+// console.log(ukraine);
+
+// console.log(usa instanceof Country);
+// console.log(usa instanceof Array);
+
+// Arguments - spread - rest
+
+function someFunc(a) {
+  return arguments[1];
 }
 
-Country.prototype.getInfo = function () {
-  return `Capital is ${this.capital}`;
+const funcExp = function (a) {
+  // const arr = Array.from(arguments);
+  // arr.forEach((el) => console.log(el));
+  return arguments;
 };
 
-const ukraine = new Country();
-const usa = new Country('USA', 'Washington');
+// const arrowFunc = (a) => arguments; // no this, no arguments, no prototype
 
-console.log(ukraine.getInfo());
-console.log(usa.getInfo());
+const arrowFunc = (a, ...args) => args; 
 
-console.log(ukraine.getInfo === usa.getInfo);
 
-console.log(ukraine);
+// console.dir(someFunc)
+// console.dir(funcExp)
+// console.dir(arrowFunc)
 
-console.log(usa instanceof Country);
-console.log(usa instanceof Array);
+// console.log(someFunc(1, 2, true, {}));
+// console.log(arrowFunc(1));
+
+// console.log(arrowFunc(1, 2, true, {}));
 
 
